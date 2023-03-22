8de7f459c6b5676b116537b3fa2e4ed20068837e
ik012982i9 <jurrutiabe22dw@ikzubirimanteo.com>
2023-03-22 14:09:11 +0100

    Punto 5

--- src/test/java/com/pruebas/SmartPhoneServiceTest.java
+++ src/test/java/com/pruebas/SmartPhoneServiceTest.java
@@ -13,8 +13,16 @@ public class SmartPhoneServiceTest {
 
         assertNotNull(count, "El método count() no debe devolver NULL");
         assertTrue(count > 0, "El valor devuelto por count() debe ser mayor que 0");
-        //assertEquals(3, count, "El valor devuelto por count() debe ser 3");
+        assertEquals(3, count, "El valor devuelto por count() debe ser 3");
         // Para que falle
-        assertEquals(4, count, "El valor devuelto por count() debe ser 3");
+        //assertEquals(4, count, "El valor devuelto por count() debe ser 3");
+    }
+
+    @Test
+    void testFindOne() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            SmartPhoneServiceImpl service = new SmartPhoneServiceImpl();
+            service.findOne(null);
+        }, "findOne() debe lanzar IllegalArgumentException si el ID es nulo");
     }
 }
